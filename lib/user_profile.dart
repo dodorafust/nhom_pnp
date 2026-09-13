a5f3c32ac803e6ff4473cfbdd50420e12f27a386
dodorafust <phanphatdz@gmail.com>
2026-09-13 08:12:21 +0700

    ok

---
+++ lib/user_profile.dart
@@ -0,0 +1,24 @@
+class UserProfile {
+  String name;
+  double weight; // kg
+  double height; // mét
+
+  UserProfile({required this.name, required this.weight, required this.height});
+
+  // Thuật toán tính BMI và phân loại
+  String calculateBMI() {
+    if (height <= 0 || weight <= 0) return "Dữ liệu không hợp lệ";
+    
+    double bmi = weight / (height * height);
+    String status;
+    
+    if (bmi < 18.5) {
+      status = "Thiếu cân";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      status = "Bình thường";
+    } else {
+      status = "Thừa cân";
+    }
+    return "BMI: \({bmi.toStringAsFixed(1)} -\)status";
+  }
+}
